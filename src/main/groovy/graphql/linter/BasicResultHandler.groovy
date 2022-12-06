a02b6ef7736d514dfa598c62772bf19385377310
kirpi4ik <dumitru.ciubenco@gmail.com>
2022-12-06 16:16:50 +0200

    Reformat basic print handler

--- src/main/groovy/graphql/linter/BasicResultHandler.groovy
+++ src/main/groovy/graphql/linter/BasicResultHandler.groovy
@@ -2,30 +2,31 @@ package graphql.linter
 
 import graphql.linter.exception.RuleValidationError
 
-class BasicResultHandler implements ResultHandler{
-    def handle(executionEnvironment, context){
+class BasicResultHandler implements ResultHandler {
+    def handle(executionEnvironment, context) {
         boolean hasErrors = context.errors.any { error -> error.value.any { it.level == FailLevel.ERROR } }
         if (hasErrors) {
             def failedRules = context.errors.findAll { error -> error.value.any { it.level == FailLevel.ERROR } }
             throw new RuleValidationError(failedRules)
         }
         boolean hasWarnings = context.errors.any { error -> error.value.any { it.level == FailLevel.WARN } }
+        int rowLength = 160
         if (hasWarnings) {
-            printf("#######################################################################################################################%n")
-            printf("###################################################    FAILURES   #####################################################%n")
-            printf("#######################################################################################################################%n")
+            printf "${'#' * rowLength}%n"
+            printf "${'#' * (rowLength / 2 - 5)} FAILURES ${'#' * (rowLength / 2 - 5)}%n"
+            printf "${'#' * rowLength}%n"
             def failedRules = context.errors.findAll { error -> error.value.any { it.level == FailLevel.WARN } }
             failedRules.each { ruleName, ruleWarn ->
-                printf("#---------------------------------------------------------------------------------------------------------------------#%n")
-                printf "# [ %-30s ] [ %-3d failures][%-6d runs] [%-4.3f sec/rule]                                    #%n", ruleName, ruleWarn.size(), context.execution[ruleName]['count'], context.execution[ruleName]['time']
-                ruleWarn.each { error -> printf("#            +  %-100s  #%n", error.message)
+                printf "#${'=' * (rowLength-2)}#%n"
+                printf "# * %-35s  [ %-3d failures][%-6d runs] [%-3.3f sec/rule]${' ' * (rowLength - 87)}#%n", ruleName, ruleWarn.size(), context.execution[ruleName]['count'], context.execution[ruleName]['time']
+                ruleWarn.each { error -> printf("#            +  %-${rowLength - 19}s  #%n", error.message)
                 }
-                def failWhenExceed = executionEnvironment.config['rules'][ruleName as String]?['failWhenExceed']
+                def failWhenExceed = executionEnvironment.config['rules'][ruleName as String] ?['failWhenExceed']
                 if (executionEnvironment.config['failWhenExceedWarningMax'] && (failWhenExceed && ruleWarn.size() > failWhenExceed)) {
                     throw new RuleValidationError("[${ruleName}] Exceeded number of failures. ${ruleWarn.size()} > ${failWhenExceed} ")
                 }
             }
-            printf("#######################################################################################################################%n")
+            printf "${'#' * rowLength}%n"
         }
     }
 }
